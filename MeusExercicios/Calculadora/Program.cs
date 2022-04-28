57674c5c55af5c114ce97dd6d79fa0f4c50f9345
Lilian <liliansacon@gmail.com>
2022-04-28 20:22:27 -0300

    Calculadora

---
+++ MeusExercicios/Calculadora/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Desenvolver uma calculadora
+             * que vai ler um numero
+             * depois vai ler o segundo numero
+             * depois vai ler o operador
+             * como resposta o programa vai
+             * responder o resultado da
+             * operacao
+             */
+
+            double numero1;
+            double numero2;
+            string operador;
+
+            // ler o primeiro numero
+            Console.WriteLine("Digite o Numero 1:");
+            double.TryParse(Console.ReadLine(), out numero1);
+            // ler o segundo numero
+            Console.WriteLine("Digite o Numero 2:");
+            double.TryParse(Console.ReadLine(), out numero2);
+            // ler o operador
+            Console.WriteLine("Digite o operador (+, -, *, /):");
+            operador = Console.ReadLine();
+            // verificar o tipo do operador
+            // realizar a operacao conforme o tipo verificado
+            // imprimir o resultado
+            if (operador == "+")
+            {
+                Console.WriteLine("Soma = {0}", numero1 + numero2);
+            }
+            if (operador == "-")
+            {
+                Console.WriteLine("Subtração = {0}", numero1 - numero2);
+            }
+            if (operador == "*")
+            {
+                Console.WriteLine("Multiplicação = {0}", numero1 * numero2);
+            }
+            if (operador == "/" && numero2 == 0)
+            {
+                Console.WriteLine("Não foi possível executar o calculo");
+            }
+            if (operador == "/")
+            {
+             if (numero2 != 0)
+                Console.WriteLine("Divisão = {0}", numero1 / numero2);
+            }
+            if (operador != "+" && operador != "-" && operador != "*" && operador != "/")
+            {
+                Console.WriteLine("Operador incorreto");
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
