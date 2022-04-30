4884f133ebb46b0fe877f646ace9b7b62ba97d39
Lilian <liliansacon@gmail.com>
2022-04-29 21:34:02 -0300

    Extremamentebasico

---
+++ MeusExercicios/Beecrowd_Extremamentebasico/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Beecrowd_Extremamentebasico
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            int resultado;
+            Int32.TryParse(Console.ReadLine(), out a);
+            Int32.TryParse(Console.ReadLine(), out b);
+
+            resultado = a + b;
+            Console.WriteLine("X = " + resultado);
+
+
+
+
+
+        }
+    }
+}
