2901a06c288f2662d7da6675d6ad6f83a9a40244
Lilian <liliansacon@gmail.com>
2022-05-02 20:27:31 -0300

    salariocombonus

---
+++ MeusExercicios/Beecrowd_SalarioComBonus/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Beecrowd_SalarioComBonus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            double salario;
+            double vendas;
+
+            nome = Console.ReadLine();
+            double.TryParse(Console.ReadLine(), out salario);
+            double.TryParse(Console.ReadLine(), out vendas);
+            {
+                Console.WriteLine("TOTAL = R$ {0}",(salario + (vendas * 15) / 100).ToString("F2"));
+            }
+
+
+
+        }
+    }
+}
