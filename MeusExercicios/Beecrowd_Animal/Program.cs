306a10f912540f37f03cf24a738918116a49f1bc
Lilian <liliansacon@gmail.com>
2022-05-02 12:10:45 -0300

    animal

---
+++ MeusExercicios/Beecrowd_Animal/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Beecrowd_Animal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Entrada1;
+            string Entrada2;
+            string Entrada3;
+            Console.WriteLine("");
+            Entrada1 = Console.ReadLine();
+            Console.WriteLine("");
+            Entrada2 = Console.ReadLine();
+            Console.WriteLine("");
+            Entrada3 = Console.ReadLine();
+
+            if (Entrada1 == ("vertebrado"))
+            {
+                if (Entrada2 == ("ave"))
+                {
+                    if (Entrada3 == ("carnivoro"))
+                    {
+                        Console.WriteLine("aguia");
+                    }
+                    else
+                    {
+                        Console.WriteLine("pomba");
+                    }
+                }
+                else
+                {
+                    if (Entrada3 == ("onivoro"))
+                    {
+                        Console.WriteLine("homem");
+                    }
+                    else
+                    {
+                        Console.WriteLine("vaca");
+                    }
+                }
+            }
+            else
+            {
+                if (Entrada2 == ("inseto"))
+                {
+                    if (Entrada3 == ("hematofago")) { Console.WriteLine("pulga"); }
+                    else
+                    {
+                        Console.WriteLine("lagarta");
+                    }
+                }
+                else
+                {
+                    if (Entrada3 == ("hematofago")) { Console.WriteLine("sanguessuga"); }
+                    else { Console.WriteLine("minhoca"); }
+                }
+            }
+        }
+
+                    
+                    
+                
+            
+        
+    }
+}
