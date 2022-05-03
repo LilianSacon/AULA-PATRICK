51442fb0b48abfd808f4e262e8895b6a81475b25
Guest 03 <guest03@alunos.sc.senac.br>
2022-05-03 19:29:02 -0300

    exemplos

---
+++ ExemplosVariados/Exemplos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exemplos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
