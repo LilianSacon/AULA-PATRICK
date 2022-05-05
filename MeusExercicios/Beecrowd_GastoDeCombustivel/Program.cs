a26e6fdeb9f13c7be70955682e09ffae29626c11
Lilian <liliansacon@gmail.com>
2022-05-05 20:19:20 -0300

    Gasto combustivel

---
+++ MeusExercicios/Beecrowd_GastoDeCombustivel/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Beecrowd_GastoDeCombustivel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tempo;
+            int velocidade;
+            double media = 12;
+            double litros;
+
+            int.TryParse(Console.ReadLine(), out tempo);
+            int.TryParse(Console.ReadLine(), out velocidade);
+
+            litros = (tempo * velocidade) / media;
+            Console.WriteLine(litros.ToString("F3")
+                                    );
+        }
+    }
+}
