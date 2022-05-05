2ea202f9d5e60bd6498ce4b3ffa175b638e355e5
Lilian <liliansacon@gmail.com>
2022-05-05 19:54:42 -0300

    Consumo

---
+++ MeusExercicios/Beecrowd_Consumo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Beecrowd_Consumo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int X;
+            double Y;
+            double consumo;
+           
+            int.TryParse(Console.ReadLine(), out X);
+            double.TryParse(Console.ReadLine(), out Y);
+
+            consumo = (X / Y);
+            Console.WriteLine(consumo.ToString("F3") + " km/l");
+
+
+        }
+    }
+}
