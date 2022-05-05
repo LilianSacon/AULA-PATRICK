262f8531fd6de6e56a7c7148a6e66fa2cf1cb2d9
Lilian <liliansacon@gmail.com>
2022-05-05 19:45:28 -0300

    esfera

---
+++ MeusExercicios/Beecrowd_Esfera/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Beecrowd_Esfera
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double PI = 3.14159;
+            double raio;
+            double area;
+            double.TryParse(Console.ReadLine(), out raio);
+
+            area = (4/3.0)*PI*Math.Pow(raio, 3);
+            Console.WriteLine("VOLUME =" + area.ToString("F3"));
+            
+        }
+    }
+}
