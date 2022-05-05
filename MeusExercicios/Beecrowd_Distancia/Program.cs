3ea9b32201a85a5c17db4b515e1bb208525d5584
Lilian <liliansacon@gmail.com>
2022-05-05 20:09:17 -0300

    Distancia

---
+++ MeusExercicios/Beecrowd_Distancia/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Beecrowd_Distancia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Km;
+            int tempo;
+
+            int.TryParse(Console.ReadLine(), out Km);
+            tempo = (Km * 2) / 1;
+            Console.WriteLine(tempo + " minutos");
+
+        }
+    }
+}
