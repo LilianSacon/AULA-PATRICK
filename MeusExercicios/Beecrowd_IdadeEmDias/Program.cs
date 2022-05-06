4473b796814631b26134cd92f6fff1d0fbaac129
Lilian <liliansacon@gmail.com>
2022-05-05 21:24:04 -0300

    Idade em dias

---
+++ MeusExercicios/Beecrowd_IdadeEmDias/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Beecrowd_IdadeEmDias
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+            int.TryParse(Console.ReadLine(), out idade);
+
+            int ano = (int)Math.Truncate(idade / 365.0);
+            idade = idade - (ano * 365);
+            Console.WriteLine(ano + " ano(s)");
+
+            int meses = (int)Math.Truncate(idade / 30.0);
+            idade = idade - (meses * 30);
+            Console.WriteLine(meses + " mes(es)");
+
+            int dias = (int) idade;
+            idade = idade - dias;
+            Console.WriteLine(dias + " dia(s)");
+
+        }
+    }
+}
