a30d8c28361de06e662bf4a28b9afc4ee0d79b06
Lilian <liliansacon@gmail.com>
2022-05-06 21:46:14 -0300

    numeros positivos

---
+++ Exemplo/funcao/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace funcao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ MeusExercicios/Beecrowd_NumerosPositivos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Beecrowd_NumerosPositivos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero1;
+            int numero2 = 0;
+            int i;
+
+            for (i = 0; i < 6; i++)
+            {
+                double.TryParse(Console.ReadLine(), out numero1);
+                if (numero1 > 0)
+                {
+                    numero2++;
+                }
+            }
+
+             Console.WriteLine(numero2 + " valores positivos"); }
+            
+        }
+    }
+
+           
+                
