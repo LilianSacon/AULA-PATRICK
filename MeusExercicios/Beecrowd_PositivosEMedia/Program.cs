de9575bba7029daf1537d5b28955f03cea499666
Lilian <liliansacon@gmail.com>
2022-05-08 20:58:15 -0300

    positivos e media

---
+++ MeusExercicios/Beecrowd_PositivosEMedia/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Beecrowd_PositivosEMedia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double Entrada = 0;
+            int Quantidade = 0;
+            double Media = 0;
+            int i;
+            double totalentradas = 0;
+
+            for (i = 0; i < 6; i++)
+            {
+                double.TryParse(Console.ReadLine(), out Entrada);
+                if (Entrada > 0)
+                {
+                    Quantidade++;
+                    totalentradas = totalentradas + Entrada;
+                }
+            }
+            Console.WriteLine(Quantidade + " valores positivos");
+
+            Media = totalentradas / Quantidade;
+            Console.WriteLine(Media.ToString("F1"));
+        
+        
+        }
+
+    }
+}
