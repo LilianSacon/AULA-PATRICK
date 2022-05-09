729c8a49ebd44d3ef005d0ee2ffefdd29118a66a
Lilian <liliansacon@gmail.com>
2022-05-09 16:34:09 -0300

    alteracoes

---
+++ MeusExercicios/Beecrowd_numerosimpares/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Beecrowd_numerosimpares
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
