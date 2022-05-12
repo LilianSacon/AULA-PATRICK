e4370fb8df9e2eb4973c31d1bae1c5b7caf4fa26
Lilian <liliansacon@gmail.com>
2022-05-11 22:15:39 -0300

    Quadrado de pares

---
+++ MeusExercicios/Beecrowd_Quadradodepares/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Beecrowd_Quadradodepares
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            int contador;
+            double numero;
+
+            int.TryParse(Console.ReadLine(), out N);
+
+            for (contador = 1; contador <= N; contador ++)
+                
+            {
+                if (contador % 2 == 0)
+                {
+                    numero = Math.Pow(contador, 2);
+                    Console.WriteLine(contador + "^2 = " +numero);
+                }
+            }
+        }
+    }
+}
