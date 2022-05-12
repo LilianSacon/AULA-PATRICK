83eed87d9020014aa635e08bc3f4902436d9c4fe
Lilian <liliansacon@gmail.com>
2022-05-11 22:49:58 -0300

    PUM

---
+++ MeusExercicios/Beecrowd_Pum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Beecrowd_Pum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            int linha;
+            int numero = 0;
+            string PUM;
+            
+            
+            int.TryParse(Console.ReadLine(), out N);
+
+            for (linha = 1; linha <= N; linha++)
+            {
+                PUM = "";
+                for (int i = 0; i < 3; i++)
+                {
+                    numero++;
+                    if (PUM != "")
+                        PUM = PUM + " ";
+
+                    PUM = PUM + numero;
+                }
+                Console.WriteLine(PUM + " PUM");
+                numero++;
+            }
+                           
+        }
+    }
+}
