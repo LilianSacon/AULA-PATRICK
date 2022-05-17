4734b709b3111da360f2dd162918c583f29ad049
Lilian <liliansacon@gmail.com>
2022-05-17 20:36:09 -0300

    primos

---
+++ MeusExercicios/Beecrowd_primos/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Beecrowd_primos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            int X;
+            int contador = 0;
+            bool primo = true;
+
+            int.TryParse(Console.ReadLine(), out N);
+
+            for (int j = 0; j < N; j++)
+            {
+                contador = 0;
+                int.TryParse(Console.ReadLine(), out X);
+
+                for (int i = 1; i <= X; i++)
+                {
+                    if (X % i == 0)
+                        contador++;
+                }
+                if (contador == 2)
+                {
+                    primo = true;
+                    Console.WriteLine(X + " eh primo");
+                }
+                else
+                {
+                    Console.WriteLine(X + " nao eh primo");
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
--- MeusExercicios/jogovelha/Program.cs
+++ MeusExercicios/jogovelha/Program.cs
@@ -13,25 +13,154 @@ namespace jogovelha
 
             InicializarTabuleiro();
             
+            int contador = 0;
+
             int quemjoga = 1;
             bool continuarjogando = true;
+            bool checarGanhador;
+            bool checarEmpate;
+            string posicaoJogada;
 
             while (continuarjogando) //laço do jogo
             {
+
                 //imprimir tabuleiro - console.writeline
                 ImprimirTabuleiro();
+
                 //ler quem joga - quemjoga alterna entre jogador 1 e 2 - sempre que jogada for impar ou par é um jogador
+                posicaoJogada = LerPosicaoJogador(quemjoga);
+
                 //registrar posição de jogada - ABC 123 só continua se registrou posição jogada 
+                RegistrarPosicaoJogador(quemjoga, posicaoJogada);
+
                 //checar se ganhou - se não ganhou ver se tem posição livre para jogar
+                checarGanhador = ChecarGanhador(quemjoga);
+
+
                 //checar empate - 
+                if(checarGanhador == false)
+                {
+                    checarEmpate = ChecarEmpate();
+                    if(checarEmpate == true)
+                    {
+                        Console.WriteLine("Houve Empate!");
+                        continuarjogando = false;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Jogador {quemjoga} é o vencedor!");
+                    continuarjogando=false;
+                }
+
                 //alterar quem joga
-                continuarjogando = false;
+                if(quemjoga == 1)
+                {
+                    quemjoga = 2;
+                }
+                else  if(quemjoga == 2)
+                {
+                    quemjoga = 1;
+                }
+
+
+                //continuarjogando = false;
             }
+            ImprimirTabuleiro();
+        }
+
+        static bool ChecarEmpate()
+        {
+            for(int i = 0; i < linhas; i++)
+            {
+                for (int j = 0; j < colunas; j++)
+                {
+                    if (jogo[i,j] == 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        static bool ChecarGanhador(int quemjoga)
+        {
+            if (jogo[0, 0] == quemjoga && jogo[0, 1] == quemjoga && jogo[0, 2] == quemjoga)
+            {
+                return true;
+            }
+            else if (jogo[1, 0] == quemjoga && jogo[1, 1] == quemjoga && jogo[1, 2] == quemjoga)
+            {
+                return true;
+            }
+            else if (jogo[2, 0] == quemjoga && jogo[2, 1] == quemjoga && jogo[2, 2] == quemjoga)
+            {
+                return true;
+            }
+            else if (jogo[0, 0] == quemjoga && jogo[1, 0] == quemjoga && jogo[2, 0] == quemjoga)
+            {
+                return true;
+            }
+            else if (jogo[0, 1] == quemjoga && jogo[1, 1] == quemjoga && jogo[2, 1] == quemjoga)
+            {
+                return true;
+            }
+            else if (jogo[0, 2] == quemjoga && jogo[1, 2] == quemjoga && jogo[2, 2] == quemjoga)
+            {
+                return true;
+            }
+            else if (jogo[0, 0] == quemjoga && jogo[1, 1] == quemjoga && jogo[2, 2] == quemjoga)
+            {
+                return true;
+            }
+            else if (jogo[0, 2] == quemjoga && jogo[1, 1] == quemjoga && jogo[2, 0] == quemjoga)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static void RegistrarPosicaoJogador(int quemjoga, string posicaoJogada)
+        {
+            int x, y;
+            var posicoes = posicaoJogada.Split(' ');
+            int.TryParse(posicoes[0], out x);
+            int.TryParse(posicoes[1], out y);
+
+            jogo[x, y] = quemjoga;
+
+        }
+
+        static string LerPosicaoJogador(int quemjoga)
+        {
+            Console.WriteLine($"Jogador {quemjoga} é a sua vez! Escolha uma posição:");
+            string linha;
+            int x, y;
+            /*
+             * 0 0 | 0 1 | 0 2
+             * 1 0 | 1 1 | 1 2
+             * 2 0 | 2 1 | 2 2
+             */
+            linha = Console.ReadLine(); //0 2
+            var posicoes = linha.Split(' ');
+            int.TryParse(posicoes[0], out x);
+            int.TryParse(posicoes[1], out y);
+
+            if(jogo[x,y] ==0)
+            {
+                // posição não jogada
+                return linha;
+            }
+            return "";
         }
         static void ImprimirTabuleiro()
         {
-            Console.Write("Tabuleiro\r\n");
-            
+            Console.WriteLine("Tabuleiro"); 
+
             for (int i = 0; i < linhas; i++)
                 
             {
@@ -63,16 +192,16 @@ namespace jogovelha
                 }
             }
         }
-            static void InicializarTabuleiro()
+         static void InicializarTabuleiro()
+        {
+            for (int i = 0; i < linhas; i++)
             {
-                for (int i = 0; i < linhas; i++)
+                for (int j = 0; j < colunas; j++)
                 {
-                    for (int j = 0; j < colunas; j++)
-                    {
-                        jogo[i, j] = 0;
-                    }
+                    jogo[i, j] = 0;
                 }
             }
+        }
         
     }
 }
