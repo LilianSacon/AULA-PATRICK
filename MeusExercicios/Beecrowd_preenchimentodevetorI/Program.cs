9a4177d7c1a3b2a2a1c5986565b3fe4f8b30328a
Lilian <liliansacon@gmail.com>
2022-05-17 21:50:42 -0300

    Preenchimento de Vetor I

---
+++ MeusExercicios/Beecrowd_preenchimentodevetorI/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Beecrowd_preenchimentodevetorI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] N = new int[10];
+            int Entrada;
+            int i;
+
+            int.TryParse(Console.ReadLine(), out Entrada);
+            N[0] = Entrada;
+            
+            for (i = 1; i < 10; i++)
+            { 
+                N[i] = N[i - 1] * 2; 
+            }
+            for (i = 0; i < 10; i++)
+            {
+                   Console.WriteLine("N[" + i + "] = " + N[i]);
+            }
+        }
+    }
+}
---
+++ MeusExercicios/CampoMinado/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CampoMinado
+{
+    internal class Program
+    {
+        static int[,] jogo;
+        const int linhas = 8, colunas = 6;
+        static void Main(string[] args)
+        {
+            jogo = new int[linhas, colunas];
+            Random numero = new Random();
+
+            InicializarTabuleiro();
+        }
+        static void InicializarTabuleiro()
+        {
+            for (int i = 0; i < linhas; i++)
+            {
+                for (int j = 0; j < colunas; j++)
+                {
+                    jogo[i, j] = 0;
+                }
+            }
+        }
+    }
+}
