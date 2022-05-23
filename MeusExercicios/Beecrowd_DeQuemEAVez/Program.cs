1ab32d29f440c979df5460581f4e328a6ee9bbec
Lilian <liliansacon@gmail.com>
2022-05-22 22:53:23 -0300

    De Quem é a Vez?

---
+++ MeusExercicios/Beecrowd_DeQuemEAVez/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Beecrowd_DeQuemEAVez
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int QT;
+            int i;
+            double total;
+            string linha1;
+            string linha2;
+            
+            int.TryParse(Console.ReadLine(), out QT);
+
+            for (i = 0; i < QT ; i++)
+            {
+                linha1 = Console.ReadLine();
+                var texto = linha1.Split(' ');
+
+                linha2 = Console.ReadLine();
+                var valores = linha2.Split(' ');
+
+                total = double.Parse(valores[0]) + double.Parse(valores[1]);
+
+                if (total % 2 == 0)
+                {
+                    if (texto[1] == "PAR")
+                        Console.WriteLine(texto[0]);
+                    else
+                        Console.WriteLine(texto[2]);
+
+                }
+                else
+                {
+                    if (texto[1] == "IMPAR")
+                        Console.WriteLine(texto[0]);
+                    else
+                        Console.WriteLine(texto[2]);
+
+                }
+
+            }
+
+        }
+    }
+}
