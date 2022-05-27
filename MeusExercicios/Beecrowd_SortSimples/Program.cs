7339368e91020ea0095e2a702630e8ace9fd95c7
Lilian <liliansacon@gmail.com>
2022-05-27 09:38:23 -0300

    Sort Simples

--- MeusExercicios/Beecrowd_SortSimples/Program.cs
+++ MeusExercicios/Beecrowd_SortSimples/Program.cs
@@ -2,21 +2,47 @@
 
 namespace Beecrowd_SortSimples
 {
-	internal class Program
-	{
-		static void Main(string[] args)
-		{
-			int[] numeros = new int[3];
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase;
+            int valor1, valor2, valor3, j;
 
-			for (int i = 0; i < numeros.Length; i++)
-			{
-				Console.Write("");
-				numeros[i] = Convert.ToInt32(Console.ReadLine());
-			}
+            frase = Console.ReadLine();
+            var vetorLinha = frase.Split(' ');
 
-			Array.Sort(numeros);
-			Console.Write("\n\n");
-			foreach (int p in numeros) Console.WriteLine(p);
-		}
-	}
+            valor1 = int.Parse(vetorLinha[0]);
+            valor2 = int.Parse(vetorLinha[1]);
+            valor3 = int.Parse(vetorLinha[2]);
+
+            for (j = 0; j < 3; j++)
+            {
+                if ((valor1 < valor2) && (valor1 < valor3))
+                {
+                    Console.WriteLine(valor1);
+                    valor1 = 999999999;
+                }
+
+                if ((valor2 < valor1) && (valor2 < valor3))
+                {
+                    Console.WriteLine(valor2);
+                    valor2 = 999999999;
+                }
+
+                if ((valor3 < valor1) && (valor3 < valor2))
+                {
+                    Console.WriteLine(valor3);
+                    valor3 = 999999999;
+                }
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine(vetorLinha[0]);
+            Console.WriteLine(vetorLinha[1]);
+            Console.WriteLine(vetorLinha[2]);
+        }
+
+    }
 }
