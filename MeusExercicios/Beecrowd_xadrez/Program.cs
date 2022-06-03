a5bc90f8ac777284b8c025bd6bcde8a6130306c8
Lilian <liliansacon@gmail.com>
2022-06-02 21:00:32 -0300

    Xadrez

---
+++ MeusExercicios/Beecrowd_xadrez/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Beecrowd_xadrez
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int l;
+            int c;
+
+            int.TryParse(Console.ReadLine(), out l);
+            int.TryParse(Console.ReadLine(), out c);
+
+            if ((l + c) % 2 == 0)
+            {
+                Console.WriteLine("1");
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+
+        }
+    }
+}
