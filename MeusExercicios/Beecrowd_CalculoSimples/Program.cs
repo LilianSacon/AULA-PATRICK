3b8e5e772ae34c249aef90e98c2798cef34e1bff
Lilian <liliansacon@gmail.com>
2022-06-12 22:16:36 -0300

    .

---
+++ MeusExercicios/Beecrowd_CalculoSimples/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Beecrowd_CalculoSimples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string peca;
+            int valor1;
+            int valor2;
+            double valor3;
+
+            peca = Console.ReadLine();
+            var vetorLinha = peca.Split(' ');
+
+            valor1 = int.Parse(vetorLinha[0]);
+            valor2 = int.Parse(vetorLinha[1]);
+            valor3 = double.Parse(vetorLinha[2]);
+
+            Console.WriteLine(valor3 * valor2);
+        }
+    }
+}
